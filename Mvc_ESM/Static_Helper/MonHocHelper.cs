2a001964fe93cea7fd86b290d4a68e6e001f748d
NguyenHoangDuy <duynguyenvl@gmail.com>
2012-10-06 10:31:55 +0700

    update bộ lọc TeacherController

---
+++ Mvc_ESM/Static_Helper/MonHocHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvc_ESM.Static_Helper
+{
+    public class MonHocHelper
+    {
+        public static String Khoa = "";
+        public static String SearchString = "Linux";
+        public static String BoMon = "";
+    }
+}
