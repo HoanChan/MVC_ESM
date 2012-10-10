6ded1955d37e9df4d816ed5d75da2bf5660313ce
HoanChan <Le.Hoan.Chan@gmail.com>
2012-10-10 16:48:59 +0700

    Design sơ bộ trang Options
    Update Jquery sang bản 1.9.0
    Sửa lại _Datatable.lib, thêm _Jquery.lib

---
+++ Mvc_ESM/Controllers/OptionsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_ESM.Controllers
+{
+    public class OptionsController : Controller
+    {
+        //
+        // GET: /Options/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
