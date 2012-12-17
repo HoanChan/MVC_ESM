4ef507c1f397c955d4185ee1e69f7a88e41d86e8
NguyenHoangDuy <duynguyenvl@gmail.com>
2012-12-17 13:22:24 +0700

    update lịch mở cửa phòng theo biểu mẫu

--- Model/DKMHModel.Context.cs
+++ Model/DKMHModel.Context.cs
@@ -10,8 +10,6 @@
 namespace Model
 {
     using System;
-    using System.ComponentModel;
-    using System.ComponentModel.DataAnnotations;
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
     
@@ -26,8 +24,7 @@ namespace Model
         {
             throw new UnintentionalCodeFirstException();
         }
-
-       
+    
         public DbSet<bomon> bomons { get; set; }
         public DbSet<CaThi> CaThis { get; set; }
         public DbSet<chuyennganh> chuyennganhs { get; set; }
